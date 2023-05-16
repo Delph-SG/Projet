09f51c46f2fac3cfae2a553a11ea7dc83bb5bcc6
Delph-SG <penelope.seraye@gmail.com>
2023-05-16 22:43:56 +0200

    Deux écran de fait

---
+++ app/src/main/java/com/example/projet/ecran_connexion.java
@@ -0,0 +1,14 @@
+package com.example.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ecran_connexion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ecran_connexion);
+    }
+}
