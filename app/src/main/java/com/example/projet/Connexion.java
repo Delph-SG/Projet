c79c4b8d29452f2fa0e2d05ea531fa186c0e89c1
Delph-SG <penelope.seraye@gmail.com>
2023-06-04 19:33:23 +0200

    ....

---
+++ app/src/main/java/com/example/projet/Connexion.java
@@ -0,0 +1,10 @@
+package com.example.projet;
+
+public class Connexion {
+
+    // Class pour enregistrer l'etat de connexion d'un utilisateur
+    public void Etat_Co() {
+        String login;
+        int connecT; // indique si l'utilisateur du login est connecté
+    }
+}
--- app/src/main/java/com/example/projet/Ecran_acceuil.java
+++ app/src/main/java/com/example/projet/Ecran_acceuil.java
@@ -2,7 +2,9 @@ package com.example.projet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Ecran_acceuil extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class Ecran_acceuil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecran_acceuil);
+
+        Button co_opt = findViewById(R.id.connex_option);
+
+        Intent option = new Intent(this, option_frag.class);
+        Intent connexion = new Intent(this, ecran_connexion.class);
+
+        int etat_co = 0; //pour enregistrer si il y a une connexion (0 = pas de connextion)
+
+        if (etat_co > 0) {
+
+            co_opt.setOnClickListener(view -> startActivity(option)); // si l'utilisateur est connecté, il a accés aux options
+        }
+        else {
+            co_opt.setOnClickListener(view -> startActivity(connexion)); // si l'utilisateur n'est pas connecté, il a accés à l'écran de connexion
+        }
+
     }
 }
