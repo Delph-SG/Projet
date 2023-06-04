ac951ff8c4a79f3782c73550f5d2a0286c916f69
Delph-SG <penelope.seraye@gmail.com>
2023-06-04 21:08:12 +0200

    bou

--- app/src/main/java/com/example/projet/crea_compte_entreprise.java
+++ app/src/main/java/com/example/projet/crea_compte_entreprise.java
@@ -1,7 +1,12 @@
 package com.example.projet;
 
+import static android.widget.Toast.LENGTH_SHORT;
+import static com.example.projet.R.string.bla;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class crea_compte_entreprise extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class crea_compte_entreprise extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crea_compte_entreprise);
+
+        Button val = findViewById(R.id.btn_val_entr);
+
+        val.setOnClickListener(view -> Toast.makeText(this, bla, LENGTH_SHORT).show());
+
+
+
     }
 }
--- app/src/main/java/com/example/projet/crea_compte_interim.java
+++ app/src/main/java/com/example/projet/crea_compte_interim.java
@@ -1,7 +1,12 @@
 package com.example.projet;
 
+import static android.widget.Toast.*;
+import static com.example.projet.R.string.bla;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class crea_compte_interim extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class crea_compte_interim extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crea_compte_interim);
+
+        Button val = findViewById(R.id.btn_val_inte);
+
+        val.setOnClickListener(view -> Toast.makeText(this, bla, LENGTH_SHORT).show());
+
     }
 }
